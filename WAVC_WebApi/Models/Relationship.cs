cd056bbfb6f3790e12f88411e5fe9bb0cf24c138
Vixmit <pawel.kolakovski@gmail.com>
2019-09-01 13:10:55 +0200

    Add project files.

---
+++ WAVC_WebApi/Models/Relationship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAVC_WebApi.Models
+{
+    public class Relationship
+    { 
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public string RelatedUserId { get; set; }
+        public virtual ApplicationUser RelatedUser { get; set; }
+    }
+}
