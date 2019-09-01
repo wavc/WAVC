3fbc0642a5a519f95420c71b8673f5bfcd5cbdc1
Vixmit <pawel.kolakovski@gmail.com>
2019-09-02 00:05:28 +0200

    Add Login and Registration view

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,37 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeftBoxComponent } from './left-box/left-box.component';
-import { NavBarComponent } from './left-box/nav-bar/nav-bar.component';
-import { FriendsListComponent } from './left-box/friends-list/friends-list.component';
-import { FriendsListElementComponent } from './left-box/friends-list/friends-list-element/friends-list-element.component';
-import { RightBoxComponent } from './right-box/right-box.component';
-import { HeaderBarComponent } from './right-box/header-bar/header-bar.component';
-import { DialogBoxComponent } from './right-box/dialog-box/dialog-box.component';
-import { SenderBarComponent } from './right-box/sender-bar/sender-bar.component';
-import { MessageRecievedComponent } from './right-box/dialog-box/message-recieved/message-recieved.component';
-import { MessageSentComponent } from './right-box/dialog-box/message-sent/message-sent.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { LogInPageComponent } from './log-in-page/log-in-page.component';
+import { MessengerPageComponent } from './messenger-page/messenger-page.component';
+import { LeftBoxComponent } from './messenger-page/left-box/left-box.component';
+import { NavBarComponent } from './messenger-page/left-box/nav-bar/nav-bar.component';
+import { FriendsListComponent } from './messenger-page/left-box/friends-list/friends-list.component';
+import { FriendsListElementComponent } from './messenger-page/left-box/friends-list/friends-list-element/friends-list-element.component';
+import { RightBoxComponent } from './messenger-page/right-box/right-box.component';
+import { HeaderBarComponent } from './messenger-page/right-box/header-bar/header-bar.component';
+import { DialogBoxComponent } from './messenger-page/right-box/dialog-box/dialog-box.component';
+import { SenderBarComponent } from './messenger-page/right-box/sender-bar/sender-bar.component';
+import { MessageRecievedComponent } from './messenger-page/right-box/dialog-box/message-recieved/message-recieved.component';
+import { MessageSentComponent } from './messenger-page/right-box/dialog-box/message-sent/message-sent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegistrationPageComponent,
+    LogInPageComponent,
+    MessengerPageComponent,
     LeftBoxComponent,
-    NavBarComponent,
-    FriendsListComponent,
-    FriendsListElementComponent,
-    RightBoxComponent,
-    HeaderBarComponent,
-    DialogBoxComponent,
-    SenderBarComponent,
-    MessageRecievedComponent,
     MessageSentComponent,
+    MessageRecievedComponent,
+    SenderBarComponent,
+    DialogBoxComponent,
+    HeaderBarComponent,
+    RightBoxComponent,
+    FriendsListElementComponent,
+    FriendsListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/src/app/messenger-page/messenger-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messenger-page',
+  templateUrl: './messenger-page.component.html',
+  styleUrls: ['./messenger-page.component.css']
+})
+export class MessengerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
