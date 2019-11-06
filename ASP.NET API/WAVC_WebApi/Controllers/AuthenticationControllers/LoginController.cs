6787192ecd75dbc9d9b3c013e30a437180f87d2b
vvenomer <vvenomer@gmail.com>
2019-11-06 22:14:53 +0100

    hidden password in registration form and automatic logging in on succesful account creation

--- ASP.NET API/WAVC_WebApi/Controllers/AuthenticationControllers/LoginController.cs
+++ ASP.NET API/WAVC_WebApi/Controllers/AuthenticationControllers/LoginController.cs
@@ -37,33 +37,12 @@ namespace WAVC_WebApi.Controllers.AuthenticationControllers
 
             if (user == null || await _userManager.CheckPasswordAsync(user, model.Password) == false)
                 return BadRequest(new { message = "Username or password is incorrect." });
-
-            string token = GenerateToken(user);
+            
+            string token = new TokenGenerator(_applicationSettings.JWTSecret).GenerateToken(user);
 
             return Ok(new { token });
 
         }
 
-        private string GenerateToken(ApplicationUser user)
-        {
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                    {
-                        new Claim(ClaimTypes.Name, user.Id),
-                        new Claim(ClaimTypes.NameIdentifier, user.Id)
-                    }),
-
-                Expires = DateTime.UtcNow.AddHours(6),
-                SigningCredentials = new SigningCredentials(
-                                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_applicationSettings.JWTSecret)),
-                                SecurityAlgorithms.HmacSha256Signature)
-            };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
-            var token = tokenHandler.WriteToken(securityToken);
-
-            return token;
-        }
     }
 }
