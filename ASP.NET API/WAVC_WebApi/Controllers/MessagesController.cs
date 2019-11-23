650057408ec5420947b309df113ecd97fb524047
vvenomer <vvenomer@gmail.com>
2019-11-23 02:59:33 +0100

    Implemented file upload for chat in backend

--- ASP.NET API/WAVC_WebApi/Controllers/MessagesController.cs
+++ ASP.NET API/WAVC_WebApi/Controllers/MessagesController.cs
@@ -11,6 +11,7 @@ using WAVC_WebApi.Data;
 using WAVC_WebApi.Hubs;
 using WAVC_WebApi.Hubs.Interfaces;
 using WAVC_WebApi.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace WAVC_WebApi.Controllers
 {
@@ -36,7 +37,7 @@ namespace WAVC_WebApi.Controllers
         public async Task<IActionResult> GetAllMessagesForConversationAsync(int conversationId)
         {
             var user = await _userManager.FindByIdAsync(User.Identity.Name);
-            
+
             if (!_dbContext.ApplicationUserConversations
                 .Where(auc => auc.ConversationId == conversationId)
                 .Any(auc => auc.UserId == user.Id))
@@ -46,7 +47,7 @@ namespace WAVC_WebApi.Controllers
 
             var messages = _dbContext.Messages.Where(m => m.ConversationId == conversationId);
             var messageModels = messages.Select(m => new MessageModel(m));
-                
+
             return Ok(messageModels);
         }
 
@@ -54,40 +55,73 @@ namespace WAVC_WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> SendMessageAsync([FromBody] MessageModel messageModel)
         {
-            var conversation = await _dbContext.Conversations.FindAsync(messageModel.ConversationId);
             var senderUser = await _userManager.FindByIdAsync(User.Identity.Name);
+
+            var result = await SaveMessageAsync(messageModel, senderUser, Message.Type.Text);
+            if (!result)
+                return BadRequest();
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("Files/{id:int}")]
+        public async Task<IActionResult> SendFileMessageAsync([FromForm]IEnumerable<IFormFile> files, [FromRoute] int id)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var message = new MessageModel() { 
+                ConversationId = id, 
+                Content= GetFileNamesAsString(files, "|")
+            };
+
+            var result = await SaveMessageAsync(message, user, Message.Type.File);
+            if (!result)
+                return BadRequest();
+
+            foreach (var file in files)
+            {
+                await file.SaveFileAsync("conversations/" + id + "/" + file.FileName);
+            }
+            return Ok();
+        }
+
+        private string GetFileNamesAsString(IEnumerable<IFormFile> files, string separator)
+        {
+            return files.Aggregate("", (s, f) => s + (string.IsNullOrEmpty(s) ? "" : separator) + f.FileName);
+        }
+
+        private async Task<bool> SaveMessageAsync(MessageModel messageModel, ApplicationUser user, Message.Type type)
+        {
+            var conversation = await _dbContext.Conversations.FindAsync(messageModel.ConversationId);
 
             if (conversation == null)
             {
-                return BadRequest();
+                return false;
             }
-
             if (!_dbContext.ApplicationUserConversations
                 .Where(auc => auc.ConversationId == conversation.ConversationId)
-                .Any(auc => auc.UserId == senderUser.Id))
+                .Any(auc => auc.UserId == user.Id))
             {
-                return BadRequest();
+                return false;
             }
-
             var msg = new Message()
             {
                 ConversationId = messageModel.ConversationId,
-                MessageType = Message.Type.Text,
-                SenderUserId = senderUser.Id,
+                MessageType = type,
+                SenderUserId = user.Id,
                 Content = messageModel.Content
             };
-
             await _dbContext.Messages.AddAsync(msg);
             await _dbContext.SaveChangesAsync();
 
-            messageModel.Type = Message.Type.Text;
-            messageModel.senderId = senderUser.Id;
+            messageModel.Type = type;
+            messageModel.senderId = user.Id;
 
             await _messagesHubContext.Clients
                 .Group(conversation.ConversationId.ToString())
                 .MessageSent(conversation.ConversationId, messageModel);
-
-            return Ok();
+            return true;
         }
     }
 }
---
+++ ASP.NET API/WAVC_WebApi/PhysicalFileProviderAdapter.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace WAVC_WebApi
+{
+    public class PhysicalFileProviderAdapter : IFileProvider
+    {
+        PhysicalFileProvider _provider;
+        static char[] _pathSeparators = new char[2]
+            { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+
+        static char[] _invalidFileNameChars = Path.GetInvalidFileNameChars().Where(character =>
+                character != Path.DirectorySeparatorChar && character != Path.AltDirectorySeparatorChar
+            ).ToArray();
+
+        public PhysicalFileProviderAdapter(string path)
+        {
+            _provider = new PhysicalFileProvider(path);
+        }
+
+        internal static bool PathNavigatesAboveRoot(string path)
+        {
+            StringTokenizer stringTokenizer = new StringTokenizer(path, _pathSeparators);
+            int num = 0;
+            foreach (StringSegment stringSegment in stringTokenizer)
+            {
+                if (!stringSegment.Equals(".") && !stringSegment.Equals(""))
+                {
+                    if (stringSegment.Equals(".."))
+                    {
+                        --num;
+                        if (num == -1)
+                            return true;
+                    }
+                    else
+                        ++num;
+                }
+            }
+            return false;
+        }
+
+        private string GetFullPath(string path)
+        {
+            if (PathNavigatesAboveRoot(path))
+            {
+                return null;
+            }
+
+            string fullPath;
+            try
+            {
+                fullPath = Path.GetFullPath(Path.Combine(_provider.Root, path));
+            }
+            catch
+            {
+                return null;
+            }
+
+            if (!IsUnderneathRoot(fullPath))
+            {
+                return null;
+            }
+
+            return fullPath;
+        }
+
+        private bool IsUnderneathRoot(string fullPath)
+        {
+            return fullPath.StartsWith(_provider.Root, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            if (string.IsNullOrEmpty(subpath) || subpath.IndexOfAny(_invalidFileNameChars) != -1)
+            {
+                return new NotFoundFileInfo(subpath);
+            }
+
+            // Relative paths starting with leading slashes are okay
+            subpath = subpath.TrimStart(_pathSeparators);
+
+            // Absolute paths not permitted.
+            if (Path.IsPathRooted(subpath))
+            {
+                return new NotFoundFileInfo(subpath);
+            }
+
+            var fullPath = GetFullPath(subpath);
+            if (fullPath == null)
+            {
+                return new NotFoundFileInfo(subpath);
+            }
+
+            var fileInfo = new FileInfo(fullPath);
+
+            return new PhysicalFileInfo(fileInfo);
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return _provider.GetDirectoryContents(subpath);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            return _provider.Watch(filter);
+        }
+    }
+}
